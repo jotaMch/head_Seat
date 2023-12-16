6cc6fa9bfead496395b38faa08c547be5d7e13e6
Jânderson Machado <jandersonmachado090@gmail.com>
2023-12-16 20:54:38 -0300

    new title

--- src/components/purchase/BuyProduct.jsx
+++ src/components/purchase/BuyProduct.jsx
@@ -22,7 +22,7 @@ const BuyProduct = () => {
                 },
                 body: JSON.stringify(formData),
             });
-        
+
             if (response.ok) {
                 console.log("Dados enviados com sucesso!");
                 // Faça o que for necessário após o envio bem-sucedido
@@ -36,67 +36,67 @@ const BuyProduct = () => {
         };
 
 return(       
-<div className="w-10/12 mx-auto"  >
-    <h2 className="text-white text-center
-    w-full text-[30px] my-10">To purchase</h2>
-    <form onSubmit={(e) => {
-            e.preventDefault();
-            SendDoc();
-        }} 
-    method="POST" 
-    className="flex flex-col mx-auto
-    justify-center items-center w-11/12" >
-        <div className="w-full mb-10">
-            <input type="text"
-            name="name"
-            className="w-full h-10 bg-transparent rounded-3xl
-            border border-sky-500 text-white px-4"
-            placeholder="your name complete"             
-            onChange={handleChange}/>
-        </div>
-        <div className="w-full mb-10">
-            <input type="email"
-            name="email"
-            className="w-full h-10 bg-transparent rounded-3xl
-            border border-sky-500 text-white
-            px-4"
-            placeholder="your email" 
-            onChange={handleChange}/>
-        </div>
-        <div className="w-full mb-10">
-            <input type="text"
-            name="address"
-            className="w-full h-10 bg-transparent rounded-3xl
-            border border-sky-500 text-white
-            px-4"
-            placeholder="your current address"
-            onChange={handleChange} />
-        </div>
-        <div className="w-full mb-10">
-            <select className="w-full h-10 text-neutral-400 rounded-3xl bg-transparent border border-sky-500 text-white px-4"
-            name="payment" onChange={handleChange}>
-                <option value="padrao" className="text-white bg-[#080808]">
-                    Select
-                </option>
-                <option value="credit-card" className="text-white bg-[#080808]">
-                    Credit card
-                </option>
-                <option value="debit-card" className="text-white bg-[#080808]">
-                    Debit Card
-                </option>
-                <option value="pix" className="text-white bg-[#080808]">
-                    Pix
-                </option>
-                <option value="ticket" className="text-white bg-[#080808]">
-                    Ticket
-                </option>
-            </select>
-        </div>
-        <button type="submit" className="border border-sky-500 rounded-3xl w-full h-14 mt-10 hover:text-sky-500 bg-sky-500 text-white hover:bg-transparent">
-            Concluded
-        </button>                            
-    </form>
-</div>
+    <div className="w-10/12 mx-auto"  >
+        <h2 className="text-white text-center
+        w-full text-[30px] my-10">Data for purchase</h2>
+        <form onSubmit={(e) => {
+                e.preventDefault();
+                SendDoc();
+            }} 
+        method="POST" 
+        className="flex flex-col mx-auto
+        justify-center items-center w-11/12" >
+            <div className="w-full mb-10">
+                <input type="text"
+                name="name"
+                className="w-full h-10 bg-transparent rounded-3xl
+                border border-sky-500 text-white px-4"
+                placeholder="your name complete"             
+                onChange={handleChange}/>
+            </div>
+            <div className="w-full mb-10">
+                <input type="email"
+                name="email"
+                className="w-full h-10 bg-transparent rounded-3xl
+                border border-sky-500 text-white
+                px-4"
+                placeholder="your email" 
+                onChange={handleChange}/>
+            </div>
+            <div className="w-full mb-10">
+                <input type="text"
+                name="address"
+                className="w-full h-10 bg-transparent rounded-3xl
+                border border-sky-500 text-white
+                px-4"
+                placeholder="your current address"
+                onChange={handleChange} />
+            </div>
+            <div className="w-full mb-10">
+                <select className="w-full h-10 text-neutral-400 rounded-3xl bg-transparent border border-sky-500 text-white px-4"
+                name="payment" onChange={handleChange}>
+                    <option value="padrao" className="text-white bg-[#080808]">
+                        Select
+                    </option>
+                    <option value="credit-card" className="text-white bg-[#080808]">
+                        Credit card
+                    </option>
+                    <option value="debit-card" className="text-white bg-[#080808]">
+                        Debit Card
+                    </option>
+                    <option value="pix" className="text-white bg-[#080808]">
+                        Pix
+                    </option>
+                    <option value="ticket" className="text-white bg-[#080808]">
+                        Ticket
+                    </option>
+                </select>
+            </div>
+            <button type="submit" className="border border-sky-500 rounded-3xl w-full h-14 mt-10 hover:text-sky-500 bg-sky-500 text-white hover:bg-transparent">
+                Concluded
+            </button>                            
+        </form>
+    </div>
     )
 }
 
