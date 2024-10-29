069022548d6c3faeebc20ee4866d1e481f5052ed
Jânderson Machado <jandersonmachado090@gmail.com>
2024-10-28 23:30:42 -0300

    add new state

--- src/components/content/Form.jsx
+++ src/components/content/Form.jsx
@@ -11,16 +11,16 @@ const FormContact = ({form}) => {
             < Header />
             <div className="p-4 msgSm w-6/12  p-4">
                 <form className={`flex flex-col w-full h-full text-white`}>
-                    <input type="text" placeholder="your name" className=" h-14 w-full pl-2 rounded bg-transparent" />
-                    <input type="email" placeholder="your email" className=" h-14 w-full pl-2 rounded bg-transparent" />
-                    <textarea name="message" placeholder="your message" 
+                    <input type="text" placeholder="seu nome" className=" h-14 w-full pl-2 rounded bg-transparent" />
+                    <input type="email" placeholder="seu email" className=" h-14 w-full pl-2 rounded bg-transparent" />
+                    <textarea name="message" placeholder="sua mensagem" 
                     className=" h-14 w-full pl-2 rounded bg-transparent
                     resize-none" 
                     />
                     <button
                     className="flex justify-center items-center 
                                 bg-lime-500 rounded-3xl text-[#000] h-14 mt-8" 
-                    type="submit">Send < RiSendPlaneFill className="ml-2" /></button>
+                    type="submit">Enviar < RiSendPlaneFill className="ml-2" /></button>
                 </form>
             </div>
         </div>
